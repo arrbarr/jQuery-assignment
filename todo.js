867b6b4295fc887cdb9bd6fdebba82e7daf0c9fe
Arttu <48829078+arrbarr@users.noreply.github.com>
2019-12-16 20:50:59 +0200

    Version 1.0
    jQuery assignment for Javascript course

---
+++ todo.js
@@ -0,0 +1,23 @@
+﻿//funktio jossa haetaan käyttäjän syöte ja siirretään se li elementteihin
+function uusiTehtävä() {
+//määritetään muuttujat, ensiksi sille mitä käyttäjä kirjoittaa, toiseksi li elementtien luomiselle
+	var tehtävä = $('#syöte').val();
+	var listaItem = $("<li>" + tehtävä +"</li>");
+	$("#lista").slideUp().fadeIn(100);
+//tarkistetaan onko syötekenttä tyhjä kun käyttäjä painaa nappia, jos niin, tulee alert tyyppinen hälytys
+  if (tehtävä == "" || tehtävä.length == 0 || tehtävä == null){
+	alert('Kenttä ei voi olla tyhjä!');
+	}
+//jos syöte ei ole tyhjä niin luodaan jQueryn appendillä uusi listaItem
+	else{
+	$("ul").append(listaItem);
+//seuraavaksi tyhjennetään käyttäjän syöttämä teksti tekstikentästä
+	$('#syöte').val("");
+//sitten jQueryn click kuuntelija joka poistaa klikatun listaItem:in
+	$("li").click(poistaTehtävä);
+	}
+};
+//funktio joka hoitaa listaItemien poistamisen
+function poistaTehtävä(e) {
+	e.target.parentElement.removeChild(e.target);
+}
