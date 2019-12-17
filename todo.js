cdb463c9cd78d1265fc66089ee3386931d3ecd61
Arttu <48829078+arrbarr@users.noreply.github.com>
2019-12-18 00:52:26 +0200

    Update todo.js
    Slight tweaks

--- todo.js
+++ todo.js
@@ -3,7 +3,8 @@ function uusiTehtävä() {
 //määritetään muuttujat, ensiksi sille mitä käyttäjä kirjoittaa, toiseksi li elementtien luomiselle
 	var tehtävä = $('#syöte').val();
 	var listaItem = $("<li>" + tehtävä +"</li>");
-	$("#lista").slideUp().fadeIn(100);
+//animaatio listaitemien lisäykseen
+	$("#lista").slideUp(50).fadeIn();
 //tarkistetaan onko syötekenttä tyhjä kun käyttäjä painaa nappia, jos niin, tulee alert tyyppinen hälytys
   if (tehtävä == "" || tehtävä.length == 0 || tehtävä == null){
 	alert('Kenttä ei voi olla tyhjä!');
